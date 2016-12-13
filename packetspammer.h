c409f029b273cb37e58cd9a6d41b55628cbc2a78
JackWindows <fhb1992@gmail.com>
2016-12-12 19:10:20 -0500

    Tried to change radiotap header in generated packet, turns out to be useless
    short/long preamble flag will be ignored (long preamble is always used)
    OFDM/CCK flag in Channel flags will be ignored(OFDM is always used)

--- packetspammer.h
+++ packetspammer.h
@@ -26,3 +26,61 @@ typedef u32 __le32;
 
 #define	MAX_PENUMBRA_INTERFACES 8
 
+struct radiotap_hdr {
+	uint8_t revision;
+	uint8_t padding;
+	uint16_t length;
+	//Present flags
+	uint8_t TSFT:1;
+	uint8_t Flags:1;
+	uint8_t Rate:1;
+	uint8_t Channel:1;
+	uint8_t FHSS:1;
+	uint8_t dBmAntennaSignal:1;
+	uint8_t dBmAntennaNoise:1;
+	uint8_t LockQuality:1;
+	uint8_t TXAttenuation:1;
+	uint8_t dbTXAttenuation:1;
+	uint8_t dBmTXPower:1;
+	uint8_t Antenna:1;
+	uint8_t dBAntennaSignal:1;
+	uint8_t dbAntennaNoise:1;
+	uint8_t RXflags:1;
+	uint8_t not_used:3;
+	uint8_t ChannelPlus:1;
+	uint8_t MCSinfo:1;
+	uint8_t A_MPDUStatus:1;
+	uint8_t VHTinfo:1;
+	uint8_t Reserved:7;
+	uint8_t RadiotapNSnext:1;
+	uint8_t VendorNSnext:1;
+	uint8_t Ext:1;
+	//Flags
+	uint8_t Flags_CFP:1;
+	uint8_t Flags_Preamble:1;	//0 for long, 1 for short
+	uint8_t Flags_WEP:1;
+	uint8_t Flags_Fragmentation:1;
+	uint8_t Flags_FCS_at_end:1;
+	uint8_t Flags_DataPad:1;
+	uint8_t DataPad:1;
+	uint8_t BadFCS:1;
+	uint8_t ShortGI:1;
+
+	uint8_t DataRate;
+	uint16_t Channel_frequency;
+	//begin channel flags
+	uint8_t cf_not_used:4;
+	uint8_t cf_Turbo:1;
+	uint8_t cf_CCK:1;
+	uint8_t cf_OFDM:1;
+	uint8_t cf_2GHz:1;
+	uint8_t cf_5GHz:1;
+	uint8_t cf_Passive:1;
+	uint8_t cf_Dynamic_CCK_OFDM:1;
+	uint8_t cf_GFSK:1;
+	uint8_t cf_GSM:1;
+	uint8_t cf_StaticTurbo:1;
+	uint8_t cf_10MHz:1;
+	uint8_t cf_5MHz:1;
+} __attribute__ ((packed)) ;
+typedef struct radiotap_hdr radiotap_hdr_t;
